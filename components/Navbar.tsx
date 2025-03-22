543ec0b0c35b0ac6722f18c5dc96e90349fe8316
clearmos <109252905+clearmos@users.noreply.github.com>
2025-03-22 00:04:26 +0000

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/manifesto">Manifestos</a></li>
+        <li><a href="/systems">Systems</a></li>
+      </ul>
+    </nav>
+  );
+}
 
