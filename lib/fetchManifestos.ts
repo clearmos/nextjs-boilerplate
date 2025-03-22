48c9050467941ede110c906af3477f99e220efef
clearmos <109252905+clearmos@users.noreply.github.com>
2025-03-22 00:02:33 +0000

    Update fetchManifestos.ts

--- lib/fetchManifestos.ts
+++ lib/fetchManifestos.ts
@@ -1,1 +1,8 @@
+import supabase from "./supabase";
+
+export async function getManifestos() {
+  let { data, error } = await supabase.from("manifesto").select("*");
+  if (error) throw error;
+  return data;
+}
 
