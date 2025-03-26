6dba47387645e0c3da181cf1ec021a2e56821221
clearmos <ediceias12@gmail.com>
2025-03-26 22:00:04 +0000

    Update page.tsx

--- app/manifesto/id/page.tsx
+++ app/manifesto/id/page.tsx
@@ -1,7 +1,13 @@
 import { getSupabaseClient } from '@/lib/supabase'
 const supabase = getSupabaseClient()
 
-export default async function ManifestoDetail({ params }: { params: { id: string } }) {
+type PageProps = {
+  params: {
+    id: string
+  }
+}
+
+export default async function ManifestoDetail({ params }: PageProps) {
   const { data, error } = await supabase.from("manifesto").select("*").eq("id", params.id).single();
   if (error) return <p>Error loading manifesto</p>;
 
